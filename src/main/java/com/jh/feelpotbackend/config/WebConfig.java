a715db314327e4a4750edd29609fe8a2a5c9fd8b
silbioa480 <silbioa480@gmail.com>
2022-02-16 10:36:32 +0900

    add allowed Methods

--- src/main/java/com/jh/feelpotbackend/config/WebConfig.java
+++ src/main/java/com/jh/feelpotbackend/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.jh.feelpotbackend.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,6 +10,13 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-      .allowedOrigins("https://silbioa480.github.io", "http://localhost:3000", "https://feel-pot-frontend.vercel.app");
+      .allowedOrigins("https://silbioa480.github.io", "http://localhost:3000", "https://feel-pot-frontend.vercel.app")
+      .allowedMethods(
+        HttpMethod.GET.name(),
+        HttpMethod.HEAD.name(),
+        HttpMethod.POST.name(),
+        HttpMethod.PUT.name(),
+        HttpMethod.DELETE.name());
+    ;
   }
 }
